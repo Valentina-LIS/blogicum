c0ae330ed2ecdfe9eea5be6c740501679a1f2e52
Валентина Лисина <lisinavalentina97@gmail.com>
2023-07-31 12:16:45 +0300

    User profile created, added pagination to User profile, updated header.html

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path('category/<slug:category_slug>/', views.category_posts,
          name='category_posts'),
+    path('profile/edit/', views.ProfileUpdateView.as_view(),
+         name='edit_profile'),
+    path('profile/<slug:username>/', views.ProfileListView.as_view(),
+         name='profile'),
 ]
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,6 +2,16 @@ import datetime
 
 from django.shortcuts import get_object_or_404, render
 
+from django.http import Http404
+
+from django.views.generic import ListView, UpdateView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib.auth.models import User
+
+from django.urls import reverse, reverse_lazy
+
 from .models import Category, Post
 
 NUM_SORT: int = 5
@@ -42,3 +52,41 @@ def category_posts(request, category_slug):
     context = {'category': category,
                'post_list': category_list}
     return render(request, template, context)
+
+
+class ProfileListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'blog/profile.html'
+    ordering = 'id'
+    paginate_by = 10
+
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        username = self.kwargs['username']
+        try:
+            profile = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise Http404
+        return Post.objects.filter(author=profile
+                                   ).order_by('-pub_date')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["profile"] = User.objects.get(username=self.kwargs['username'])
+        return context
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'blog/user.html'
+    fields = ['first_name', 'last_name', 'username', 'email']
+    success_url = reverse_lazy('blog:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_success_url(self):
+        return reverse('blog:profile', kwargs={
+            'username': self.request.user.username})
