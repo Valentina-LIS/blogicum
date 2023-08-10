9740485d0aff97a44fa8557e45f1267178b1c0c3
Валентина Лисина <lisinavalentina97@gmail.com>
2023-08-10 20:55:53 +0300

    adjusting imports in models.py

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,8 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from pages.models import BaseModel
-
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
